b0cc993896f00bfa9e7c01dbb759f51d166662ff
trung8394 <tnguyen5@uccs.com>
2021-04-25 08:57:57 +0000

    Install Rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_045932) do
+ActiveRecord::Schema.define(version: 2021_04_25_084921) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before(:all)do
+    @post = Post.new(body: "My Body", title:"My Title")
+  end
+  it "should have a matching body" do
+    expect(@post.body).to eq("My Body")
+  end
+  it "should have a matching title" do
+    expect(@post.title).to eq("My Title")
+  end
+end
