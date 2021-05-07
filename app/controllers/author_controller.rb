3125fba3472c8eabc60c79ea7ed0463910d1f3f7
trung8394 <tnguyen5@uccs.com>
2021-05-07 03:24:22 +0000

    Fixed sign in page

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,6 +1,3 @@
-class AuthorController <  ActionController::Base
-    protect_from_forgery with: :exception
-    layout 'author'
-    
+class AuthorController < ApplicationController
     before_action :authenticate_author!
 end
